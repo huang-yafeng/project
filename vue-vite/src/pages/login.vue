585048748c2450f2ca225592029957c8d05dba9e
黄亚枫 <huangy_feng@aliyun.com>
2023-04-05 15:38:09 +0800

    创建项目

---
+++ vue-vite/src/pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login">
+        login
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.login {
+    width: 300px;
+    height: 500px;
+    background: red;
+}
+</style>
