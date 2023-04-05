d57a6b8a483bea80af475bd8bbb2d56ab18ce701
黄亚枫 <huangy_feng@aliyun.com>
2023-04-05 15:44:22 +0800

    项目初始化

--- vue-vite/src/pages/login.vue
+++ vue-vite/src/pages/login.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="login">
-        login
+        <van-button type="primary">主要按钮</van-button>
+        <van-button type="success">成功按钮</van-button>
+        <van-button type="default">默认按钮</van-button>
+        <van-button type="warning">警告按钮</van-button>
+        <van-button type="danger">危险按钮</van-button>
     </div>
 </template>
 
@@ -12,6 +16,6 @@
 .login {
     width: 300px;
     height: 500px;
-    background: red;
+    /* background: red; */
 }
 </style>
--- vue-vite/vite.config.ts
+++ vue-vite/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import postcsspxtoviewport from "postcss-px-to-viewport"
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()],
+    }),],
   css: {
     postcss: {
       plugins: [
