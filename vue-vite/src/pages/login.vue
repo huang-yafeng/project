956f5dd463c6b7ce3623f110fd789b82580ea447
黄亚枫 <huangy_feng@aliyun.com>
2023-04-05 15:51:03 +0800

    提交

--- vue-vite/src/pages/login.vue
+++ vue-vite/src/pages/login.vue
@@ -1,18 +1,29 @@
 <template>
-    <div class="login">
-        <van-button type="primary">主要按钮</van-button>
-        <van-button type="success">成功按钮</van-button>
-        <van-button type="default">默认按钮</van-button>
-        <van-button type="warning">警告按钮</van-button>
-        <van-button type="danger">危险按钮</van-button>
-    </div>
+    <van-form @submit="onSubmit">
+        <van-cell-group inset>
+            <van-field v-model="username" name="用户名" label="用户名" placeholder="用户名"
+                :rules="[{ required: true, message: '请填写用户名' }]" />
+            <van-field v-model="password" type="password" name="密码" label="密码" placeholder="密码"
+                :rules="[{ required: true, message: '请填写密码' }]" />
+        </van-cell-group>
+        <div style="margin: 16px;">
+            <van-button round block type="primary" native-type="submit">
+                提交
+            </van-button>
+        </div>
+    </van-form>
 </template>
 
 <script setup lang="ts">
-
+import { ref } from 'vue';
+const username = ref('');
+const password = ref('');
+const onSubmit = (values: any) => {
+    console.log('submit', values);
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .login {
     width: 300px;
     height: 500px;
