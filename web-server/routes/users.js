55184ad99ee69aa2dabc1c5c3e79183beba9d4d4
黄亚枫 <huang_yfeng@aliyun.com>
2023-04-28 13:02:02 +0800

    提交

--- web-server/routes/users.js
+++ web-server/routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('../models/mysql')
+const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
+//登录
+router.post('/login', async function (req, resx, next) {
+  const { username, password } = req.body
+  const md5 = crypto.createHash('md5');
+  const pwd = md5.update(password).digest('hex');
+  const sql = `select * from userinfo where username="${username}" antd password="${pwd}"`
+  const userData = await mysql.query(sql);
+  console.log(userData)
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+})
 
 module.exports = router;
